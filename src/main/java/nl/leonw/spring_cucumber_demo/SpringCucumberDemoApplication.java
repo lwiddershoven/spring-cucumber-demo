33e7ebd56768299b1d8040e6aac14366eb8dcede
Leon Widdershoven <git@qaz.dds.nl>
2024-09-21 14:20:12 +0200

    Added spring-initializer starter

---
+++ src/main/java/nl/leonw/spring_cucumber_demo/SpringCucumberDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.leonw.spring_cucumber_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCucumberDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCucumberDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/leonw/spring_cucumber_demo/SpringCucumberDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.leonw.spring_cucumber_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCucumberDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
